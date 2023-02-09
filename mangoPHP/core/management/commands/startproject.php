625b9c9555bbca27f392b703e3ae4ec28c22c0ca
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-02-10 00:23:59 +0300

    Create mango project

--- mangoPHP/core/management/commands/startproject.php
+++ mangoPHP/core/management/commands/startproject.php
@@ -8,8 +8,8 @@ function startproject($projectName)
     mkdir($projectName);
 
     $settingsFile = fopen("$projectName/settings.php", "w");
-    $viewsFile = fopen("$projectName/views.php", "w");
-    $urlsFile = fopen("$projectName/urls.php", "w");
+    $viewsFile    = fopen("$projectName/views.php", "w");
+    $urlsFile     = fopen("$projectName/urls.php", "w");
 }
 
 ?>
