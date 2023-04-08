d1c04529c3b2a25c9a30c707106f948f79b3dc25
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-04-08 23:43:43 +0300

    Model

--- mangoPHP/db/model.php
+++ mangoPHP/db/model.php
@@ -1,7 +1,24 @@
 <?php
 
 class Model{
-    
+
+    public $table_name;
+    public $primaryKey;
+
+
+    function set_table_name($name) 
+    {
+        $this->$table_name = $name;
+    }
+    function get_table_name() 
+    {
+        return $this->$table_name;
+    }
+
+    function field()
+    {
+       
+    }
 }
 
 ?>
